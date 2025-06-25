a6760872e862f8402e59894039b7f9e86efe0e21
Moses <ogbopinamoses@gmail.com>
2025-06-25 14:32:54 +0100

    Add experience entry for Kontrack Technologies Inc and enhance layout

--- src/components/experience.tsx
+++ src/components/experience.tsx
@@ -2,6 +2,22 @@ import { BsArrowUpRight } from "react-icons/bs";
 
 const data = [
   {
+    date: "FEB 2024 - PRESENT",
+    role: "Frontend & Mobile Engineer",
+    company: "Kontrack Technologies Inc",
+    description: `As a Frontend & Mobile Engineer at Kontrack Technologies Inc, I have been instrumental in developing and maintaining high-quality web and mobile applications. My role involves collaborating with cross-functional teams to design user-friendly interfaces and ensure seamless functionality across platforms. I leverage my expertise in React, and React Native to create responsive and efficient applications that meet the needs of our clients. My commitment to delivering exceptional user experiences drives my work, and I continuously seek to improve the performance and security of our applications.`,
+    skills: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "React",
+      "React Native",
+      "Tailwind CSS",
+      "Expo",
+    ],
+    companyLink: "https://kontrack.ca/",
+  },
+  {
     date: "MAY 2022 - MAY 2023",
     role: "Frontend & Mobile Engineer",
     company: "Flovest Savings Limited",
@@ -22,9 +38,9 @@ const data = [
 
 export default function Experience() {
   return (
-    <div className="flex flex-col gap-4">
+    <div className="flex flex-col gap-8">
       {data?.map((item, index) => (
-        <div key={index} className="flex flex-col gap-2">
+        <div key={index} className="flex flex-col gap-1">
           <p className="text-xs lg:text-sm text-gray-400 tracking-wider">
             {item?.date}
           </p>
@@ -33,12 +49,20 @@ export default function Experience() {
             <p className="text-gray-200 text-base lg:text-lg font-bold">
               {item?.role}
             </p>
-            <p className="text-gray-400 text-base lg:text-lg flex items-center gap-2">
+            <a
+              href={item?.companyLink}
+              target="_blank"
+              className="text-gray-400 text-base lg:text-lg flex items-center gap-2 w-fit"
+            >
               {item?.company}
               <BsArrowUpRight />
-            </p>
+            </a>
+            <div className="relative flex items-center my-4"></div>
+            <div className="h-1 w-20 bg-gradient-to-r from-rose-400 via-rose-300 to-transparent rounded-full" />
+            {/* <span className="absolute left-0 -top-3 text-rose-400 text-xs font-semibold tracking-widest uppercase">
+              Experience
+            </span> */}
           </div>
-
           <p className="text-gray-300 text-base lg:text-lg">
             {item?.description}
           </p>
